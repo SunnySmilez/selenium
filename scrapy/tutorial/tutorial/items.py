0d761587563f7aa9dd6e776da126c86243d0c926
zhouzhi <zhouzhi@100tal.com>
2023-05-23 00:03:36 +0800

    scrapy tutorial

---
+++ scrapy/tutorial/tutorial/items.py
@@ -0,0 +1,12 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+#https://uk.craftdlondon.com/collections/bestsellers?page=2
+import scrapy
+
+
+class craftdlondon(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
